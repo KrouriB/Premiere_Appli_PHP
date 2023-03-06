88aa009139f42fff8209c1f152590477ca2adbdb
KrouriB <brice.krouri@gmail.com>
2023-03-06 11:37:55 +0100

    verification des donnée rentré dans le formulaire

---
+++ traitement.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+if (isset($_POST['submit'])) {
+    $name = filter_input(INPUT_POST, "name", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $price = filter_input(INPUT_POST, "price", FILTER_VALIDATE_FLOAT, FILTER_FLAG_ALLOW_FRACTION); // FILTER_FLAG_ALLOW_FRACTION -> permet l'utilisation de . ou , pour les nombre décimaux
+    $qtt = filter_input(INPUT_POST, "qtt", FILTER_VALIDATE_INT);
+    if ($name && $price && $qtt) {
+        $product = [
+            "name" => $name,
+            "price" => $price,
+            "qtt" => $qtt,
+            "total" => $price * $qtt
+        ];
+        $_SESSION['products'][] = $product;
+    }
+}
+header("Location:index.php"); // redirection vers index.php si on va sur ce fichier
+die; // forece la fin d'un script
