9ae4bc9aa30cfbb0e8295a342278042678b36756
KrouriB <brice.krouri@gmail.com>
2023-03-06 16:26:13 +0100

    création class&id et légere modif

--- index.php
+++ index.php
@@ -5,35 +5,38 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Ajout Produit</title>
 </head>
 
 <body>
-    <h1>Ajouter un produit</h1>
-    <form action="traitement.php" method="post">
-        <p>
-            <label>
-                Nom du Produit :
-                <input type="text" name="name">
-            </label>
-        </p>
-        <p>
-            <label>
-                Prix du Produit :
-                <input type="number" step="any" name="price">
-            </label>
-        </p>
-        <p>
-            <label>
-                Quantité du Produit :
-                <input type="number" name="qtt" value="1">
-            </label>
-        </p>
-        <p>
-            <input type="submit" name="submit" value="Ajouter le produit">
-        </p>
-    </form>
-    <a href="recap.php">Récap</a>
+    <div id="indexWrap">
+        <h1>Ajouter un produit</h1>
+        <form action="traitement.php" method="post">
+            <p class="info">
+                <label for="name">
+                    Nom du Produit :
+                </label>
+                <input type="text" name="name" id="name">
+            </p>
+            <p class="info">
+                <label for="price">
+                    Prix du Produit :
+                </label>
+                <input type="number" step="any" name="price" id="price">
+            </p>
+            <p class="info">
+                <label for="qtt">
+                    Quantité du Produit :
+                </label>
+                <input type="number" name="qtt" value="1" id="qtt">
+            </p>
+            <p>
+                <input type="submit" name="submit" value="Ajouter le produit" id="submitbtn">
+            </p>
+        </form>
+        <a href="recap.php">Récapitulatif des produits</a>
+    </div>
 </body>
 
 </html>
