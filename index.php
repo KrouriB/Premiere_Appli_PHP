4274973f8323f60da938af1a0f5d996d33eb8516
KrouriB <brice.krouri@gmail.com>
2023-03-07 13:52:39 +0100

    création templat a réutiliser sur toute les page

--- index.php
+++ index.php
@@ -1,55 +1,42 @@
-<?php
-session_start();
-?>
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <script src="https://kit.fontawesome.com/50e50e8630.js" crossorigin="anonymous"></script>
-    <title>Ajout Produit</title>
-</head>
-
-<body>
-    <div id="indexWrap">
-        <h1>Ajouter un produit</h1>
-        <form action="traitement.php?action=add" method="post">
-            <p class="info">
-                <label for="name">
-                    Nom du Produit :
-                </label>
-                <input type="text" name="name" id="name">
-            </p>
-            <p class="info">
-                <label for="price">
-                    Prix du Produit :
-                </label>
-                <input type="number" step="any" name="price" id="price">
-            </p>
-            <p class="info">
-                <label for="qtt">
-                    Quantité du Produit :
-                </label>
-                <input type="number" name="qtt" value="1" id="qtt">
-            </p>
-            <p>
-                <input type="submit" name="submit" value="Ajouter le produit" id="submitbtn">
-            </p>
-        </form>
-        <a href="recap.php">Récapitulatif des produits</a>
-        <?php
+<?php ob_start(); ?>
+<div id="indexWrap">
+    <h1>Ajouter un produit</h1>
+    <form action="traitement.php?action=add" method="post">
+        <p class="info">
+            <label for="name">
+                Nom du Produit :
+            </label>
+            <input type="text" name="name" id="name">
+        </p>
+        <p class="info">
+            <label for="price">
+                Prix du Produit :
+            </label>
+            <input type="number" step="any" name="price" id="price">
+        </p>
+        <p class="info">
+            <label for="qtt">
+                Quantité du Produit :
+            </label>
+            <input type="number" name="qtt" value="1" id="qtt">
+        </p>
+        <p>
+            <input type="submit" name="submit" value="Ajouter le produit" id="submitbtn">
+        </p>
+    </form>
+    <a href="recap.php">Récapitulatif des produits</a>
+    <?php
+    $valCountSession = 0;
+    if (!isset($_SESSION['products']) || empty($_SESSION['products'])) {
         $valCountSession = 0;
-        if (!isset($_SESSION['products']) || empty($_SESSION['products'])) {
-            $valCountSession = 0;
-        } else {
-            $valCountSession = count($_SESSION['products']);
-        }
-        ?>
-        <span id="indexSpan">Le nombre d'objet en session actuellement est de <?= $valCountSession ?>.</span>
-    </div>
-</body>
-
-</html>
+    } else {
+        $valCountSession = count($_SESSION['products']);
+    }
+    ?>
+    <span id="indexSpan">Le nombre d'objet en session actuellement est de <?= $valCountSession ?>.</span>
+</div>
+<?php
+    $content = ob_get_clean();
+    $titre="Ajout produit";
+    require "template.php";
+?>
--- recap.php
+++ recap.php
@@ -1,65 +1,54 @@
+<?php ob_start(); ?>
+
+<div id="recapWrap">
+    <?php
+    if (!isset($_SESSION['products']) || empty($_SESSION['products'])) {
+        echo "<p>Aucun produit en session ...</p>";
+    } else {
+    ?>
+        <table>
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Nom</th>
+                    <th>Prix</th>
+                    <th>Quantité</th>
+                    <th>Total</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                $totalGeneral = 0;
+                foreach ($_SESSION['products'] as $index => $product) {
+                ?>
+                    <tr>
+                        <td class="tdIndex"><?= $index ?></td>
+                        <td><?= $product['name'] ?></td>
+                        <td><?= number_format($product['price'], 2, ",", "&nbsp;") . "&nbsp;€" ?></td>
+                        <td class="qttLigne"><div class="btnPlusMoins"><a href="traitement.php?action=more&id=<?= $index ?>" class="btn plus">+</a><a href="traitement.php?action=less&id=<?= $index ?>" class="btn moins">-</a></div><?= $product['qtt'] ?></td>
+                        <td class="unTotal"><?= number_format($product['total'], 2, ",", "&nbsp;") . "&nbsp;€" ?></td>                        
+                        <td class="colSuppr"><a href="traitement.php?action=delete&id=<?= $index ?>"><i class="fa-solid fa-delete-left"></i></a></td>'
+                    </tr>
+                <?php
+                    $totalGeneral += $product['total'];
+                }
+                ?>
+                <tr>
+                    <td colspan=4>Total général : </td>
+                    <td id="caseTotalVal"><strong><?= number_format($totalGeneral, 2, ",", "&nbsp;") . "&nbsp;€" ?></strong></td>
+                    <td class="colSuppr"><a href="traitement.php?action=clear"><i class="fa-solid fa-trash"></i></a></td>
+                </tr>
+            </tbody>
+        </table>
+        <span id="recapSpan">Le nombre d'objet en session actuellement est de <?= count($_SESSION['products']) ?>.</span>
+    <?php
+    }
+    ?>
+    <a href="index.php" id="btnIndex">Principal</a>
+</div>
+
 <?php
-session_start();
+    $content = ob_get_clean();
+    $titre="Panier";
+    require "template.php";
 ?>
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <script src="https://kit.fontawesome.com/50e50e8630.js" crossorigin="anonymous"></script>
-    <title>Récapitulatif des produits</title>
-</head>
-
-<body>
-    <div id="recapWrap">
-        <?php
-        if (!isset($_SESSION['products']) || empty($_SESSION['products'])) {
-            echo "<p>Aucun produit en session ...</p>";
-        } else {
-        ?>
-            <table>
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>Nom</th>
-                        <th>Prix</th>
-                        <th>Quantité</th>
-                        <th>Total</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php
-                    $totalGeneral = 0;
-                    foreach ($_SESSION['products'] as $index => $product) {
-                    ?>
-                        <tr>
-                            <td class="tdIndex"><?= $index ?></td>
-                            <td><?= $product['name'] ?></td>
-                            <td><?= number_format($product['price'], 2, ",", "&nbsp;") . "&nbsp;€" ?></td>
-                            <td class="qttLigne"><div class="btnPlusMoins"><a href="traitement.php?action=more&id=<?= $index ?>" class="btn plus">+</a><a href="traitement.php?action=less&id=<?= $index ?>" class="btn moins">-</a></div><?= $product['qtt'] ?></td>
-                            <td class="unTotal"><?= number_format($product['total'], 2, ",", "&nbsp;") . "&nbsp;€" ?></td>                        
-                            <td class="colSuppr"><a href="traitement.php?action=delete&id=<?= $index ?>"><i class="fa-solid fa-delete-left"></i></a></td>'
-                        </tr>
-                    <?php
-                        $totalGeneral += $product['total'];
-                    }
-                    ?>
-                    <tr>
-                        <td colspan=4>Total général : </td>
-                        <td id="caseTotalVal"><strong><?= number_format($totalGeneral, 2, ",", "&nbsp;") . "&nbsp;€" ?></strong></td>
-                        <td class="colSuppr"><a href="traitement.php?action=clear"><i class="fa-solid fa-trash"></i></a></td>
-                    </tr>
-                </tbody>
-            </table>
-            <span id="recapSpan">Le nombre d'objet en session actuellement est de <?= count($_SESSION['products']) ?>.</span>
-        <?php
-        }
-        ?>
-        <a href="index.php" id="btnIndex">Principal</a>
-    </div>
-</body>
-
-</html>
