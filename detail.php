36966d2fd5e11d9b5e7720ccd7e3465a0dec7e22
KrouriB <brice.krouri@gmail.com>
2023-03-08 11:44:04 +0100

    affichage des info d'un produit

--- detail.php
+++ detail.php
@@ -4,7 +4,14 @@ ob_start();
 require "fonction.php";
 ?>
 
-
+<div id="detailWrap">
+    <figure>
+        <img src="<?= $_SESSION['products'][$_GET['id']]['image'] ?>" alt="image du produit <?= $_SESSION['products'][$_GET['id']]['name'] ?>">
+    </figure>
+    <h1><?= $_SESSION['products'][$_GET['id']]['name'] ?></h1>
+    <span><?= $_SESSION['products'][$_GET['id']]['price'] ?>€</span>
+    <p><?= $_SESSION['products'][$_GET['id']]['resume'] ?></p>
+</div>
 
 <?php
     $content = ob_get_clean();
